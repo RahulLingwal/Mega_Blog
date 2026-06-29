d2aeda8acece1f7f1fe8e8bfdcce4ae8c5f28926
Rahul Lingwal <rahullingwal200@gmail.com>
2026-06-29 21:38:32 +0530

    Add Header, LogoutBtn, Footer, Container and Logo

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Container from "./container";
+import Header from "./Header/header";
+import Footer from "./Footer";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+
+export { Container, Header, Footer, Logo, LogoutBtn };
