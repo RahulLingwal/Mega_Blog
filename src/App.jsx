916dc0238f84cbf61ea7ba411b352a09c8a09a2b
Rahul Lingwal <rahullingwal200@gmail.com>
2026-06-30 21:43:53 +0530

    Add Select component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import { use, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./components/index";
+import { Header, Footer } from "./components/index";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return (
-    <>
-      <div>Hello</div>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userDatat) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
