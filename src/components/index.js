c6b37002836ce780a47ac4fc094af587477e72a0
Rahul Lingwal <rahullingwal200@gmail.com>
2026-07-08 14:10:13 +0530

    fix: correct the import of ProtectedRoute

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,32 @@
-import Container from "./container";
-import Header from "./Header/header";
-import Footer from "./Footer";
+import Container from "./container/Container";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 import Logo from "./Logo";
+import Button from "./Button";
 import LogoutBtn from "./Header/LogoutBtn";
+import Select from "./Select";
+import Input from "./Input";
+import RTE from "./RTE";
+import Login from "./Login";
+import Signup from "./Signup";
+import PostCard from "./PostCard";
+import ProtectedRoute from "./AuthLayout";
+import PostForm from "./post-form/PostForm";
 
-export { Container, Header, Footer, Logo, LogoutBtn };
+export {
+  Container,
+  Header,
+  Footer,
+  Logo,
+  LogoutBtn,
+  Button,
+  Select,
+  Input,
+  RTE,
+  Login,
+  Signup,
+  PostCard,
+  PostForm,
+};
+
+export { ProtectedRoute as AuthLayout };
