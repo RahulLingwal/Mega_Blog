a739073b0ccf690c20f3c4106b226adf43434464
Rahul Lingwal <rahullingwal200@gmail.com>
2026-07-08 14:11:38 +0530

    fix: rename evn to env in env.js

--- src/env/env.js
+++ src/env/env.js
@@ -1,9 +1,9 @@
 const env = {
-  appwriteUrl: String(import.meta.evn.VITE_APPWRITE_URL),
-  appwriteProductId: String(import.meta.evn.VITE_APPWRITE_PROJECT_ID),
-  appwriteDatabaseId: String(import.meta.evn.VITE_APPWRITE_DATABASE_ID),
-  appwriteTableId: String(import.meta.evn.VITE_APPWRITE_TABLE_ID),
-  appwriteBucketId: String(import.meta.evn.VITE_APPWRITE_BUCKET_ID),
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProductId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteTableId: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 };
 
 export default env;
